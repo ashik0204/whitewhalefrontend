3d54774290fff5dfd19c9dc6a3f43a7cce971899
Ashik <ashikstenny@gmail.com>
2025-06-26 11:22:06 +0530

    debugger

--- config/api.js
+++ config/api.js
@@ -1,7 +1,7 @@
-const isDevelopment = import.meta.env.MODE === 'development';
+// const isDevelopment = import.meta.env.MODE === 'development';
 
-// Define the base URL for API calls
-const API_BASE_URL = process.env.REACT_APP_BACKEND_URL || 
-  (isDevelopment ? 'http://localhost:3001' : 'https://whitewhale-xxs6.onrender.com');
+// // Define the base URL for API calls
+// const API_BASE_URL = process.env.REACT_APP_BACKEND_URL || 
+//   (isDevelopment ? 'http://localhost:3001' : 'https://whitewhale-xxs6.onrender.com');
 
-export { API_BASE_URL };
+// export { API_BASE_URL };
